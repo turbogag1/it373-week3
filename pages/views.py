1bd012d9ed5e107c2d02c3fe0a4241a8d02e1ac0
turbogag1 <gabrielramos16969@gmail.com>
2025-10-20 03:51:26 +0800

    Added FBV-based CRUD for Post

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponseBadRequest
 from pages.models import Post
 
 # Create your views here.
@@ -25,9 +26,40 @@ def server_error_view(request):
 
 def post_list(request):
     # Models.objects.all()
-    posts = Post.objects.all().prefetch_related('comments')
+    posts = Post.objects.all()
     context = {
         'posts': posts,
         'title': 'Posts',
     }
     return render(request, 'post_list.html', context)
+
+def post_create(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        body = request.POST.get('body')
+        if not title or not body:
+            return HttpResponseBadRequest('Title and body are required.')
+        Post.objects.create(title=title, body=body)
+        return redirect('post_list')
+    return render(request, 'post_form.html')
+
+def post_view(request, pk):
+    #post = get_object_or_404(Post, pk=pk)
+    post = Post.objects.get(pk=pk)
+    return render(request, 'post_view.html', {'post': post})
+
+def post_update(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        post.title = request.POST.get('title') or post.title
+        post.body = request.POST.get('body') or post.body
+        post.save()
+        return redirect('post_view', pk=pk)
+    return render(request, 'post_form.html', {'post': post})
+
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('post_list')
+    return render(request, 'post_confirm_delete.html', {'post': post})
