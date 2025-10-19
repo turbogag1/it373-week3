761667127d1bf409a0ad915a382e03f9f713bde4
turbogag1 <gabrielramos16969@gmail.com>
2025-10-20 00:35:28 +0800

    Added student, course, enrollment model

--- pages/models.py
+++ pages/models.py
@@ -20,3 +20,29 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment by {self.author} on {self.post_id}."
+    
+class Student(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+    
+class Course (models.Model):
+    code = models.CharField(max_length=20, unique=True)
+    title = models.CharField(max_length=200)
+    students = models.ManyToManyField('Student', through='Enrollment', related_name='courses')
+
+    def __str__(self):
+        return f"{self.code} - {self.title}"
+
+class Enrollment(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    grade = models.CharField(max_length=2, blank=True)
+
+    class Meta:
+        unique_together = ('student', 'course')
+
+    def __str__(self):
+        return f"{self.student.first_name} {self.student.last_name} - {self.course.code}"
